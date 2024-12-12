e5a876ee1f1d5a40272f2b1e31cf779001bd7fc9
Samrouth <bichsamroth@gmail.com>
2024-12-12 17:43:54 +0700

    feat : Error Server side

--- frontend/src/component/card/Card.jsx
+++ frontend/src/component/card/Card.jsx
@@ -1,8 +1,12 @@
-const Card = ({ title, description }) => {
+
+const Card = ({ id , name, email ,create_date }) => {
   return (
     <div className="card">
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <h2>{name}</h2>
+      <p>{email}</p>
+      <span>{create_date}</span>
     </div>
   );
 }
+
+export default Card;
--- frontend/src/page/landing/Langding.jsx
+++ frontend/src/page/landing/Langding.jsx
@@ -1,9 +1,30 @@
-const Langding = () => {
+import React, { useState, useEffect } from "react";
+import Card from "../../component/card/Card";
+
+const Landing = () => {
+  const [users, setUsers] = useState([]);
+  // fetch data from backend
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Data",data);
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  }, []);  
+
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {users.map((user) => (
+        <Card key={user.id} id={user.id} name={user.name} email={user.email} create_date={user.create_date} />
+      ))}
     </div>
   );
 };
 
-export default Langding;
+export default Landing;
+
